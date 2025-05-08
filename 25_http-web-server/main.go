025865d8cdf6e453d60e033728e4db4dc365556e
Firdaus <firdaus1337@gmail.com>
2025-05-08 14:36:42 +0800

    read json input

--- 25_http-web-server/main.go
+++ 25_http-web-server/main.go
@@ -6,7 +6,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+type ServerInfo struct {
+	ServerName  string    `json:"serverName"`
+	Version     string    `json:"version"`
+	CurrentTime time.Time `json:"currentTime"`
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	serverInfo := ServerInfo{ServerName: "MySimpleServer", Version: "1.0.1", CurrentTime: time.Now()}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(serverInfo)
+	if err != nil {
+		log.Printf("Error encoding /api/info response: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -26,8 +49,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		if name == "" {
 			name = "World"
 		}
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		fmt.Fprintf(w, "Hello, %s! (from GET)\n", name)
+		response := map[string]string{"greeting": fmt.Sprintf("Hello, %s! (From GET)", name)}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Printf("Error encoding /name response: %v\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
 
 	case http.MethodPost:
 		type HelloRequest struct {
@@ -51,7 +80,16 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 			name = "World"
 		}
 
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		response := map[string]string{"receivedName": name, "message": fmt.Sprintf("Received POST for %s!", name)}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Printf("Error encoding /name response: %v\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
 		fmt.Fprintf(w, "Hello, %s! (from POST)\n", name)
 	default:
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -62,6 +100,7 @@ func main() {
 	addr := ":8080"
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/api/info", infoHandler)
 
 	fmt.Printf("Starting simple HTTP server on address %s\n", addr)
 	err := http.ListenAndServe(addr, nil)
