425617ee94ea86a7e375f248196f5d0bd8745187
Firdaus <firdaus1337@gmail.com>
2025-05-08 13:59:35 +0800

    read input from request

--- 24_web-crawler/go.mod
+++ 24_web-crawler/go.mod
@@ -2,4 +2,4 @@ module web-crawler
 
 go 1.24.0
 
-require golang.org/x/net v0.40.0 // indirect
+require golang.org/x/net v0.40.0
--- 25_http-web-server/main.go
+++ 25_http-web-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,14 +15,47 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintln(w, "Welcome to the Simple Server!")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintln(w, "Hello there, web wanderer!")
+	switch r.Method {
+	case http.MethodGet:
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "World"
+		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprintf(w, "Hello, %s! (from GET)\n", name)
+
+	case http.MethodPost:
+		type HelloRequest struct {
+			Name string `json:"name"`
+		}
+		defer r.Body.Close()
+		r.Body = http.MaxBytesReader(w, r.Body, 1024*1024)
+		decoder := json.NewDecoder(r.Body)
+		decoder.DisallowUnknownFields()
+
+		var req HelloRequest
+		err := decoder.Decode(&req)
+
+		if err != nil {
+			http.Error(w, "Bad request: Invalid JSON format or data", http.StatusBadRequest)
+			return
+		}
+
+		name := req.Name
+		if name == "" {
+			name = "World"
+		}
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprintf(w, "Hello, %s! (from POST)\n", name)
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
